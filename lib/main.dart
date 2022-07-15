10237a2e72c0a966bb2a65098f5a93439991e7fb
sagar-subedi <sagarxic@gmail.com>
2022-07-15 10:25:28 +0545

    git repo initialized, and container widget understood. EdgeInsets is a crucial object

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SafeArea(
+            child: Container(
+              // margin: EdgeInsets.all(30.0),
+              // margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
+              margin: EdgeInsets.fromLTRB(20, 40, 20, 40),
+              // EdgeInsets.only(left: 30) //only sets left margin
+              padding: EdgeInsets.all(20.0),
+              color: Colors.yellow,
+              height: 100.0,
+              width: 100.0,
+              child: Center(child: Text('Hello')),
+
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
