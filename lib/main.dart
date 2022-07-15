3134a83b56693d902de1913dd3c2f0e4864cf5b7
sagar-subedi <sagarxic@gmail.com>
2022-07-15 15:31:48 +0545

    Columns and Rows

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,51 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: SafeArea(
-            child: Container(
-              // margin: EdgeInsets.all(30.0),
-              // margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
-              margin: EdgeInsets.fromLTRB(20, 40, 20, 40),
-              // EdgeInsets.only(left: 30) //only sets left margin
-              padding: EdgeInsets.all(20.0),
-              color: Colors.yellow,
-              height: 100.0,
-              width: 100.0,
-              child: Center(child: Text('Hello')),
+          child: Column(
+            // verticalDirection: VerticalDirection.up,
+
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // .spaceBetween
+
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            // .start , .end , .center
+
+            // SizedBox(height: 30), //as a children will create a gap wherever it is placed between children
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width:  100.0,
+                color:  Colors.blue,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100.0,
+                width:  100.0,
+                color:  Colors.yellow,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width:  100.0,
+                color:  Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
 
             ),
           ),
         ),
-      ),
     );
   }
 }
+
+// Container(
+// // // margin: EdgeInsets.all(30.0),
+// // // margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
+// // margin: EdgeInsets.fromLTRB(20, 40, 20, 40),
+// // // EdgeInsets.only(left: 30) //only sets left margin
+// // padding: EdgeInsets.all(20.0),
+// color: Colors.yellow,
+// height: 100.0,
+// width: 100.0,
+// child: Center(child: Text('Container 1')),
