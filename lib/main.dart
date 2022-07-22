6701c70bc587d7dbafca1f3168a8bd3764b2f57b
sagar-subedi <sagarxic@gmail.com>
2022-07-22 09:43:05 +0545

    The version without cards

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            // verticalDirection: VerticalDirection.up,
-
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // .spaceBetween
-
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            // .start , .end , .center
-
-            // SizedBox(height: 30), //as a children will create a gap wherever it is placed between children
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width:  100.0,
-                color:  Colors.blue,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage("images/diamond.png")
+              ),
+              Text("Sagar Subedi",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text("FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "SourceSansPro",
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),
               ),
               Container(
-                height: 100.0,
-                width:  100.0,
-                color:  Colors.yellow,
-                child: Text('Container 2'),
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade400,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text("+977 9806604253",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      )
+                    )
+
+                  ],
+                ),
               ),
+
               Container(
-                height: 100.0,
-                width:  100.0,
-                color:  Colors.red,
-                child: Text('Container 3'),
-              ),
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade400,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text("sagarsubedi01@gmail.com",
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    )
+                    ),
+                  ],
+                ),
+              )
+
             ],
 
             ),
